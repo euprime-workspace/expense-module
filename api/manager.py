195c5813a7c613e93fbea72ddf27072c92730394
amithmalaghan <86976645+amithmalaghan@users.noreply.github.com>
2023-11-10 19:05:22 +0530

    Delete api directory

--- api/manager.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.db.models.query import QuerySet
-
-class SoftDeleteManager(models.Manager):
-    def get_queryset(self) -> QuerySet:
-        return super().get_queryset().filter(deleted_by=None)
